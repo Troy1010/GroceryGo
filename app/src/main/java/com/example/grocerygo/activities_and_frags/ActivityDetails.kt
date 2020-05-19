404a65c7d7b70cf5201fdc7ee198029c1b9fd46d
Troy Swanson <2troy2020@gmail.com>
2020-05-19 13:37:34 -0500

    ActivityDetails uses plus_minus

--- app/src/main/java/com/example/grocerygo/activities_and_frags/ActivityDetails.kt
+++ app/src/main/java/com/example/grocerygo/activities_and_frags/ActivityDetails.kt
@@ -1,6 +1,7 @@
 package com.example.grocerygo.activities_and_frags
 
 import android.os.Bundle
+import android.view.View
 import com.example.grocerygo.R
 import com.example.grocerygo.extras.*
 import com.example.grocerygo.inheritables.GGToolbarActivity
@@ -8,6 +9,7 @@ import com.example.grocerygo.models.Product
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_details.*
 import kotlinx.android.synthetic.main.includible_plus_minus.*
+import kotlinx.android.synthetic.main.includible_plus_minus.view.*
 
 class ActivityDetails : GGToolbarActivity() {
     override val title: String
@@ -16,32 +18,28 @@ class ActivityDetails : GGToolbarActivity() {
         get() = R.layout.activity_details
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        val product = intent.getSerializableExtra(KEY_PRODUCT) as Product
         super.onCreate(savedInstanceState)
+        val product = intent.getSerializableExtra(KEY_PRODUCT) as Product
         text_view_price.text = "$"+product.price.toString()
         text_view_details.text = product.description
-        var imagePath = Config.BASE_URL_ITEM_IMAGES + product.image
-        if (imagePath.isNotEmpty()) {
-            Picasso
-                    .get()
-                    .load(imagePath)
-                    .placeholder(R.drawable.not_found)
-                    .error(R.drawable.no_image_available_vector_illustration_260nw_744886198)
-                    .into(image_view)
-        }
-//        button_add_to_cart.setOnClickListener {
-//            this.easyToast("${product.productName} added to cart")
-//
-//            App.db.addProduct(product)
-//        }
+        image_view.easyPicasso(Config.BASE_URL_ITEM_IMAGES + product.image)
         text_view_number_plus_minus.text = product.quantity.toString()
+        if (product.quantity!=0) {
+            text_view_add.visibility=View.GONE
+        }
+        text_view_add.setOnClickListener {
+            text_view_add.visibility= View.GONE
+            App.db.addProduct(product)
+            text_view_number_plus_minus.text = product.quantity.toString()
+        }
         button_plus.setOnClickListener {
             App.db.addProduct(product)
             text_view_number_plus_minus.text = product.quantity.toString()
         }
         button_minus.setOnClickListener {
             if (product.quantity == 1) {
-                product.quantity = 0 // to update ui
+                text_view_add.visibility= View.VISIBLE
+                product.quantity = 0 // to update ui, probably unnecessary
                 App.db.deleteProduct(product)
             } else {
                 App.db.minusProduct(product)
