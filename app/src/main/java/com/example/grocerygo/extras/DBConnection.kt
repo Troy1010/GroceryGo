2827008d82096d7a4b13516c74763cd11d2bc4db
Troy Swanson <2troy2020@gmail.com>
2020-05-19 11:23:47 -0500

    DBConnection stores image names

--- app/src/main/java/com/example/grocerygo/extras/DBConnection.kt
+++ app/src/main/java/com/example/grocerygo/extras/DBConnection.kt
@@ -12,12 +12,13 @@ class DBConnection :
 
     companion object {
         const val DATABASE_NAME = "GroceryGoDB"
-        const val DATABASE_VERSION = 6
+        const val DATABASE_VERSION = 7
         const val TABLE_NAME = "Products"
         const val COL_PRICE = "Price"
         const val COL_NAME = "Name"
         const val COL_QUANTITY = "Quantity"
         const val COL_PRODUCT_ID = "ProductID"
+        const val COL_IMAGE_NAME = "ImageName"
     }
 
     override fun onCreate(sqlDatabase: SQLiteDatabase) {
@@ -26,9 +27,80 @@ class DBConnection :
             """create table $TABLE_NAME($COL_NAME char(50), 
                    $COL_PRICE MONEY,
                    $COL_QUANTITY INT,
-                   $COL_PRODUCT_ID char(250) 
+                   $COL_PRODUCT_ID char(250),
+                   $COL_IMAGE_NAME char(250)
                    )"""
         sqlDatabase.execSQL(sqlCreateTable)
+    }
+
+    fun addProduct(product: Product) {
+        if (hasProduct(product)) {
+            product.quantity = (getProductQuantityByProductID(product._id) ?: 0) + 1
+            updateProduct(product)
+        } else {
+            product.quantity = 1
+            val contentValues = ContentValues()
+            contentValues.put(COL_NAME, product.productName)
+            contentValues.put(COL_PRICE, product.price)
+            contentValues.put(COL_QUANTITY, product.quantity)
+            contentValues.put(COL_PRODUCT_ID, product._id)
+            contentValues.put(COL_IMAGE_NAME, product.image)
+            databaseSQL.insert(TABLE_NAME, null, contentValues)
+        }
+    }
+
+    fun updateProduct(product: Product) {
+        if (product._id == "") {
+            logz("DBConnection`Tried to use updateProduct on a product without a ProductID")
+            return
+        }
+        val whereArgs = arrayOf(product._id)
+        val whereClause = "$COL_PRODUCT_ID=?"
+        var contentValues = ContentValues()
+        contentValues.put(COL_NAME, product.productName)
+        contentValues.put(COL_PRICE, product.price)
+        contentValues.put(COL_QUANTITY, product.quantity)
+        contentValues.put(COL_PRODUCT_ID, product._id)
+        contentValues.put(COL_IMAGE_NAME, product.image)
+        databaseSQL.update(TABLE_NAME, contentValues, whereClause, whereArgs)
+    }
+
+    fun getProducts(): ArrayList<Product> {
+        val returningList = ArrayList<Product>()
+        val columns = arrayOf(COL_NAME, COL_PRICE, COL_QUANTITY, COL_PRODUCT_ID, COL_IMAGE_NAME)
+        val cursor = databaseSQL.query(TABLE_NAME, columns, null, null, null, null, null, null)
+        if (cursor != null && cursor.moveToFirst()) {
+            do {
+                returningList.add(generateProductFromPrimedCursor(cursor))
+            } while (cursor.moveToNext())
+        }
+        cursor.close()
+        return returningList
+    }
+
+    private fun generateProductFromPrimedCursor(cursor: Cursor): Product {
+        return Product(
+            productName = cursor.getString(cursor.getColumnIndex(COL_NAME)),
+            price = cursor.getDouble(cursor.getColumnIndex(COL_PRICE)),
+            quantity = cursor.getInt(cursor.getColumnIndex(COL_QUANTITY)),
+            _id = cursor.getString(cursor.getColumnIndex(COL_PRODUCT_ID)),
+            image = cursor.getString(cursor.getColumnIndex(COL_IMAGE_NAME))
+        )
+    }
+
+    //////////////// DOES NOT DEAL WITH ALL COLUMNS
+
+    fun minusProduct(product: Product) {
+        if (hasProduct(product)) {
+            product.quantity = maxOf(0, (getProductQuantityByProductID(product._id) ?: 0) - 1)
+            if (product.quantity == 0 ) {
+                deleteProduct(product)
+            } else {
+                updateProduct(product)
+            }
+        } else {
+            logz("DBConnection`minusProduct`attempted to subtract quantity from a product which is not in DB.")
+        }
     }
 
     fun getProductByProductID(_id: String): Product? {
@@ -50,72 +122,6 @@ class DBConnection :
             return null
         }
     }
-
-    fun addProduct(product: Product) {
-        if (hasProduct(product)) {
-            product.quantity = (getProductQuantityByProductID(product._id) ?: 0) + 1
-            updateProduct(product)
-        } else {
-            product.quantity = 1
-            val contentValues = ContentValues()
-            contentValues.put(COL_NAME, product.productName)
-            contentValues.put(COL_PRICE, product.price)
-            contentValues.put(COL_QUANTITY, product.quantity)
-            contentValues.put(COL_PRODUCT_ID, product._id)
-            databaseSQL.insert(TABLE_NAME, null, contentValues)
-        }
-    }
-
-    fun minusProduct(product: Product) {
-        if (hasProduct(product)) {
-            product.quantity = maxOf(0, (getProductQuantityByProductID(product._id) ?: 0) - 1)
-            if (product.quantity == 0 ) {
-                deleteProduct(product)
-            } else {
-                updateProduct(product)
-            }
-        } else {
-            logz("DBConnection`minusProduct`attempted to subtract quantity from a product which is not in DB.")
-        }
-    }
-
-    fun updateProduct(product: Product) {
-        if (product._id == "") {
-            logz("DBConnection`Tried to use updateProduct on a product without a ProductID")
-            return
-        }
-        val whereArgs = arrayOf(product._id)
-        val whereClause = "$COL_PRODUCT_ID=?"
-        var contentValues = ContentValues()
-        contentValues.put(COL_NAME, product.productName)
-        contentValues.put(COL_PRICE, product.price)
-        contentValues.put(COL_QUANTITY, product.quantity)
-        databaseSQL.update(TABLE_NAME, contentValues, whereClause, whereArgs)
-    }
-
-    fun getProducts(): ArrayList<Product> {
-        val returningList = ArrayList<Product>()
-        val columns = arrayOf(COL_NAME, COL_PRICE, COL_QUANTITY, COL_PRODUCT_ID)
-        val cursor = databaseSQL.query(TABLE_NAME, columns, null, null, null, null, null, null)
-        if (cursor != null && cursor.moveToFirst()) {
-            do {
-                returningList.add(generateProductFromPrimedCursor(cursor))
-            } while (cursor.moveToNext())
-        }
-        cursor.close()
-        return returningList
-    }
-
-    private fun generateProductFromPrimedCursor(cursor: Cursor): Product {
-        return Product(
-            productName = cursor.getString(cursor.getColumnIndex(COL_NAME)),
-            price = cursor.getDouble(cursor.getColumnIndex(COL_PRICE)),
-            quantity = cursor.getInt(cursor.getColumnIndex(COL_QUANTITY)),
-            _id = cursor.getString(cursor.getColumnIndex(COL_PRODUCT_ID))
-        )
-    }
-
-    ////////////////
 
     fun hasProduct(product: Product): Boolean {
         val cursor = databaseSQL.rawQuery(
