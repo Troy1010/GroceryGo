943fab9b05253ea9541e9d15330c94fefee50c2b
Troy Swanson <2troy2020@gmail.com>
2020-05-25 16:25:39 -0500

    new slider images

--- app/src/main/java/com/example/grocerygo/activities_and_frags/FragHome.kt
+++ app/src/main/java/com/example/grocerygo/activities_and_frags/FragHome.kt
@@ -41,7 +41,12 @@ class FragHome : TMFragment(layout = R.layout.frag_home) {
     }
 
     private fun setupImageSlider() {
-        val images = arrayListOf<Int>(R.drawable.grocery_basket_icons_bag_food_icon, R.drawable.slider_image_two)
+        val images = arrayListOf<Int>(
+            R.drawable.si_fruitsandveggies_custom,
+            R.drawable.si_grocery_isle,
+            R.drawable.si_stock_veggies,
+            R.drawable.si_supermarketcart_main
+        )
         view_pager_image_slider.adapter = AdapterImageSlider(activity!!, images)
         view_pager_image_slider.setOnTouchListener { _, _ ->
             true
