4a6c0999d70b11b72602ec9203d2890ac1d0f949
Troy Swanson <2troy2020@gmail.com>
2020-05-25 17:00:55 -0500

    cleanup

--- app/src/main/java/com/example/grocerygo/activities_and_frags/FragHome.kt
+++ app/src/main/java/com/example/grocerygo/activities_and_frags/FragHome.kt
@@ -1,13 +1,11 @@
 package com.example.grocerygo.activities_and_frags
 
-import android.os.Handler
 import com.example.grocerygo.R
 import com.example.grocerygo.activities_and_frags.Inheritables.HostCallbacks
 import com.example.grocerygo.activities_and_frags.Inheritables.ToolbarCallbacks
-import com.example.grocerygo.adapters.AdapterImageSlider
 import com.example.grocerygo.extras.App
 import com.example.grocerygo.activities_and_frags.Inheritables.TMFragment
-import com.example.grocerygo.adapters.AdapterImageSlider2
+import com.example.grocerygo.adapters.AdapterImageSlider
 import com.smarteist.autoimageslider.IndicatorAnimations
 import com.smarteist.autoimageslider.SliderAnimations
 import kotlinx.android.synthetic.main.frag_home.*
@@ -20,16 +18,6 @@ class FragHome : TMFragment(layout = R.layout.frag_home) {
         super.onStart()
         setupParent()
         setupImageSlider()
-//        button_logout.setOnClickListener {
-//            App.sm.logout()
-//            (activity as HostCallbacks).goToProfile()
-//        }
-//        Picasso
-//                .get()
-//                .load(Endpoints.HOME_IMAGE)
-//                .placeholder(R.drawable.not_found)
-//                .error(R.drawable.no_image_available_vector_illustration_260nw_744886198)
-//                .into(image_view)
         // fake-bind text_view_hello
         text_view_hello.text = getString(R.string.hello_start, App.sm.user?.name?:"Welcome!")
     }
@@ -50,7 +38,7 @@ class FragHome : TMFragment(layout = R.layout.frag_home) {
             R.drawable.si_stock_veggies,
             R.drawable.si_supermarketcart_main
         )
-        slider_view.setSliderAdapter(AdapterImageSlider2(activity!!, images))
+        slider_view.setSliderAdapter(AdapterImageSlider(activity!!, images))
         slider_view.startAutoCycle()
         slider_view.setIndicatorAnimation(IndicatorAnimations.WORM)
         slider_view.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION)
