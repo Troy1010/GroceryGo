db84dd9c710a376e278d145edbbc54e9fca39501
Troy Swanson <2troy2020@gmail.com>
2020-05-26 13:08:02 -0500

    renaming

--- app/src/main/java/com/example/grocerygo/activities_and_frags/ActivityPayment.kt
+++ app/src/main/java/com/example/grocerygo/activities_and_frags/ActivityPayment.kt
@@ -4,9 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import com.example.grocerygo.R
 import com.example.grocerygo.activities_and_frags.Inheritables.GGToolbarActivity
-import kotlinx.android.synthetic.main.activity_c_payment.*
+import kotlinx.android.synthetic.main.activity_payment.*
 
-class ActivityPayment: GGToolbarActivity(layout = R.layout.activity_c_payment) {
+class ActivityPayment: GGToolbarActivity(layout = R.layout.activity_payment) {
     override val title: String
         get() = "Payment"
 
