f43ef0f8ba54333d90482953c5aca43755b45be5
Troy Swanson <2troy2020@gmail.com>
2020-05-26 16:35:21 -0500

    OrderReview prettified

--- app/src/main/java/com/example/grocerygo/activities_and_frags/ActivityOrderReview.kt
+++ app/src/main/java/com/example/grocerygo/activities_and_frags/ActivityOrderReview.kt
@@ -1,6 +1,7 @@
 package com.example.grocerygo.activities_and_frags
 
 import android.content.Intent
+import android.graphics.Paint
 import android.os.Bundle
 import android.view.Menu
 import android.view.View
@@ -21,11 +22,18 @@ import com.example.grocerygo.models.OrderSummary
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_c_order_review.*
 import kotlinx.android.synthetic.main.item_order_review.view.*
+import kotlinx.android.synthetic.main.z_cart_last_item.*
+import kotlinx.android.synthetic.main.z_cart_last_item.view.*
+import kotlinx.android.synthetic.main.z_cart_last_item.view.text_view_fake_discount
+import kotlinx.android.synthetic.main.z_cart_last_item.view.text_view_fake_price_total
+import kotlinx.android.synthetic.main.z_cart_last_item.view.text_view_item_quantity
+import kotlinx.android.synthetic.main.z_cart_last_item.view.text_view_price_total
+import kotlinx.android.synthetic.main.z_cart_last_item.view.text_view_shipping
+import kotlinx.android.synthetic.main.z_cart_last_item.view.text_view_tax
 import org.json.JSONObject
 
 class ActivityOrderReview : GGToolbarActivity(layout = R.layout.activity_c_order_review), AdapterRecyclerView.Callbacks {
-    override val title: String
-        get() = "Order Review"
+    override val title = "Order Review"
     lateinit var products: ArrayList<Product>
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,6 +41,24 @@ class ActivityOrderReview : GGToolbarActivity(layout = R.layout.activity_c_order
         setupClickListeners()
         refresh()
         setupRecyclerView()
+    }
+
+    private fun refresh() {
+        text_view_profile_value.text = App.sm.user?.name ?: "!WARNING: User not logged in"
+        text_view_address_value.text = App.sm.primaryAddress?.displayableStreetAddress ?: "!WARNING: Primary address not selected"
+        text_view_payment_value.text = App.sm.displayPayment?: "!WARNING: Payment method not selected"
+        //
+        products = App.db.getProducts()
+        val orderSummary = OrderSummary(products)
+        text_view_item_quantity.text = "${orderSummary.totalQuantity} item(s)"
+        text_view_fake_price_total.text = DisplayMoney(orderSummary.totalFakePrice)
+        text_view_fake_price_total.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+        text_view_fake_discount.text =
+            "${DisplayMoney(orderSummary.fakeDiscount)} discount (${orderSummary.fakeDiscountPercentage}%)"
+        text_view_price_total.text = DisplayMoney(orderSummary.totalPrice)
+        text_view_tax.text = DisplayMoney(orderSummary.tax)
+        text_view_shipping.text = DisplayMoney(orderSummary.deliveryFee)
+        text_view_grand_total_value.text = DisplayMoney(OrderSummary(App.db.getProducts()).grandTotal)
     }
 
     private fun setupClickListeners() {
@@ -49,12 +75,7 @@ class ActivityOrderReview : GGToolbarActivity(layout = R.layout.activity_c_order
     // Setup Toolbar
     override fun onPrepareOptionsMenu(menu: Menu): Boolean {
         menu.findItem(R.id.menu_cart).isVisible = false
-        return super.onPrepareOptionsMenu(menu)
-    }
-
-    private fun refresh() {
-        products = App.db.getProducts()
-        text_view_grand_total_value.text = DisplayMoney(OrderSummary(App.db.getProducts()).grandTotal)
+        return super.onPrepareOptionsMenu(menu) // TODO replace with new way of doing this
     }
 
     private fun setupRecyclerView() {
