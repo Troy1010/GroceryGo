d2dd93822e1ddb0005c3f8c85721b1e759494593
Troy Swanson <2troy2020@gmail.com>
2020-05-26 23:42:01 -0500

    Good morning, afternoon

--- app/src/main/java/com/example/grocerygo/activities_and_frags/FragHome.kt
+++ app/src/main/java/com/example/grocerygo/activities_and_frags/FragHome.kt
@@ -6,9 +6,11 @@ import com.example.grocerygo.activities_and_frags.Inheritables.ToolbarCallbacks
 import com.example.grocerygo.extras.App
 import com.example.grocerygo.activities_and_frags.Inheritables.TMFragment
 import com.example.grocerygo.adapters.AdapterImageSlider
+import com.example.grocerygo.extras.logz
 import com.smarteist.autoimageslider.IndicatorAnimations
 import com.smarteist.autoimageslider.SliderAnimations
 import kotlinx.android.synthetic.main.frag_home.*
+import java.time.LocalDateTime
 
 class FragHome : TMFragment(layout = R.layout.frag_home) {
 
@@ -18,10 +20,33 @@ class FragHome : TMFragment(layout = R.layout.frag_home) {
         super.onStart()
         setupParent()
         setupImageSlider()
-        // fake-bind text_view_hello
-        text_view_hello.text = getString(R.string.hello_start, App.sm.user?.name?:"Welcome!")
+        refresh()
     }
 
+    private fun refresh() {
+        var currentHour = java.util.Calendar.getInstance().getTime().hours
+        val name = App.sm.user?.name
+        val helloString = if (currentHour <11) {
+            if (name==null) {
+                "Good morning"
+            } else {
+                "Good morning, $name"
+            }
+        } else if (currentHour <16) {
+            if (name==null) {
+                "Hello, Welcome!"
+            } else {
+                "Hello, $name"
+            }
+        } else {
+            if (name==null) {
+                "Good afternoon"
+            } else {
+                "Good afternoon, $name"
+            }
+        }
+        text_view_hello.text = helloString
+    }
 
 
     private fun setupParent() {
