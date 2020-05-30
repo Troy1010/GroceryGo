701ac296b2b8f9e8a09c7b4a928d1379e88b4cbf
Troy Swanson <2troy2020@gmail.com>
2020-05-30 15:30:03 -0500

    investigate`EnumWithLambdas. First attempt. hacky but working

--- app/src/main/java/com/example/grocerygo/extras/DisplayAndValidationHandlers.kt
+++ app/src/main/java/com/example/grocerygo/extras/DisplayAndValidationHandlers.kt
@@ -7,3 +7,13 @@ class DisplayAndValidationHandlers {
 fun DisplayMoney(value:Double):String {
     return ("$" + "%.2f".format(value.round(2)))
 }
+
+
+
+
+
+enum class Validator(val id: Int, val lambda: ()->String) {
+    Name(0, {
+        "qwer"
+    })
+}
